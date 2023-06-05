213c51d86178ef54367233bdd7a29c08ebb7f400
nkpro2000 <45198561+nkpro2000@users.noreply.github.com>
2023-06-05 12:47:25 +0530

    Updated setup-lxr.py

--- setup-lxr.py
+++ setup-lxr.py
@@ -4,11 +4,15 @@
 # Will be run user like `python setup-lxr.py` after cloning this repo in /mnt/LinEx/Root .
 
 import os
-import re
 import sys
+import re
+import shutil
 
 LINEXROOT = '/mnt/LinEx/Root/'
-LINEXROOT = '/home/nkpro/nk/Lobby/my-exr/'
+LINEXROOT_GIT = '/var/lxr/'+ os.environ['USER'] +'/LinExRoot_git/'
+LINEXROOT_OUT = '/var/lxr/'+ os.environ['USER'] +'/LinExRoot_out/'
+
+LINEXROOT_GIT = '/home/nkpro/nk/Lobby/my-exr/'
 
 import types
 snk = types.ModuleType(
@@ -18,17 +22,27 @@ snk = types.ModuleType(
 )
 sys.modules['snk'] = snk
 PYFUNCTION = re.compile(r'(?<=\n)def .*\n(?: .*\n|\n)+')
-with open(LINEXROOT + '.sub/my-dir/setup-nk.py') as f:
+with open(LINEXROOT_GIT + '.sub/my-dir/setup-nk.py') as f:
     code = f.read()
 code = '\n\n'.join(PYFUNCTION.findall(code))
 code = compile(code, 'Part of setup-nk.py', 'exec')
 exec (
     code,
-    {**globals(), 'NK_DIR':LINEXROOT},
+    {
+        **globals(),
+        'NK_DIR':LINEXROOT_GIT, # set_folder_icon uses icons from NK_DIR+'.assets/'+icon_path .
+    },
     snk.__dict__
 )
 
+
+shutil.rmtree(LINEXROOT_OUT, ignore_errors=True)
+os.makedirs(LINEXROOT_OUT, exist_ok=True)
+
 for dir in enumerate('Tux,Wine,Darling,Droid,Boxes'.split(',')):
-    os.mkdir(f'/tmp/test/{dir[0]+1}{dir[1]}')
-    snk.set_folder_icon(f'/tmp/test/{dir[0]+1}{dir[1]}/.directory', f'{dir[0]+1}_{dir[1]}.png')
+    os.mkdir(dir_path:=LINEXROOT_OUT+f'{dir[0]+1}{dir[1]}/')
+    if os.path.isfile(LINEXROOT+f'{dir[0]+1}{dir[1]}/.directory'):
+        shutil.copyfile(LINEXROOT+f'{dir[0]+1}{dir[1]}/.directory', dir_path+'.directory')
+        # To avoid overwriting other contents in .directory file.
+    snk.set_folder_icon(dir_path+'.directory', f'{dir[0]+1}_{dir[1]}.png')
 
